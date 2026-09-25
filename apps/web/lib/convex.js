5c761b32d6701dc8aaa918fabc7911c09d34c53b
Uttkarsh Singh <singhuttkarsh450@gmail.com>
2026-09-25 09:22:08 +0530

    chore: remove Convex backend files and dependencies

--- apps/web/lib/convex.js
+++
@@ -1,6 +0,0 @@
-"use client";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
-
-export { convex, ConvexProvider };
